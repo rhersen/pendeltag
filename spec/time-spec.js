b3bd9c7aa0343d8300fd0969b8cd72ddf0028731
Rudolf Hersén <rhersen@gmail.com>
2012-10-04 07:03:05 +0200

    found workaround for the memory leak!

--- spec/time-spec.js
+++ spec/time-spec.js
@@ -2,7 +2,9 @@ var time = require('../public/modules/time');
 
 describe('time', function() {
     it('should calculate difference in seconds', function() {
-        expect(time.diff(3333, 1000)).toEqual(2.3);
+        expect(time.diff(3333, 1000).toString()).toEqual('2.333');
+        expect(time.diff(3133, 1000).toString()).toEqual('2.133');
+//        expect(time.diff(3000, 1000).toString()).toEqual('2.0');
     });
 
 });
